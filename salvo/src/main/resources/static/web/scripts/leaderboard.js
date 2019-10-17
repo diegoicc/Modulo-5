a5a3d858651443aaed9609dadb5a1365c3101ad5
g3fo <matbarreiros@gmail.com>
2019-10-17 20:31:33 -0300

    task2

--- salvo/src/main/resources/static/web/scripts/leaderboard.js
+++ salvo/src/main/resources/static/web/scripts/leaderboard.js
@@ -7,7 +7,9 @@ function leaderBoard() {
                 let tablaFormateada = addTableHTML(data);
                 let tablaScore = document.getElementById("leaderBoard");
                 tablaScore.innerHTML = tablaFormateada;
+
             }
+
 
             function addTableHTML(data) {
                 var tabla = "<thead><tr><th>Full Name</th><th>Total</th><th>Won</th><th>Lost</th><th>Tied</th></tr></thead>";
@@ -28,6 +30,7 @@ function leaderBoard() {
                 return tabla;
             }
             scoreTable(data);
+
 
         });
 }
